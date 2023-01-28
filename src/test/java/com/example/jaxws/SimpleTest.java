d44a8fc8f7ebf2ed7b7957409e371ff184d5a526
Sztzoli <bezraat@gmail.com>
2023-01-28 01:44:31 +0100

    jaxws

---
+++ src/test/java/com/example/jaxws/SimpleTest.java
@@ -0,0 +1,14 @@
+package com.example.jaxws;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleTest {
+
+    @Test
+    void dummy() {
+        String test = "Zoli";
+        assertEquals("Zoli",test);
+    }
+}
